2f1551d3be0f0a573f4e075b765af96d9524c9f6
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-09-08 17:32:28 +0600

    dbconnet

---
+++ create.js
@@ -0,0 +1,31 @@
+// 1.  npm i express cors mongodb dotenv
+// 2.  go to express+mongodb documentation copy code 
+// 3.  index.js 
+
+
+// index.js file and db connection copy code ...........................................
+// const express = require('express');
+// const cors = require('cors');
+// const { connectToServer } = require('./dbConnect');
+// const app = express();
+// const port = process.env.PORT || 5000;
+// app.use(cors());
+// app.use(express.json());
+
+
+// connectToServer((err)=>{
+
+//         if(!err){
+//         app.listen(port, () => {
+//         console.log(`listening ${port}`);
+//         })}
+//         else{
+//             console.log(err);
+//         }
+// })
+// app.get('/', (req,res) => {
+//     res.send("Hello world!! my name is mozahid i am connetted")
+// })
+
+// ..............................................................
+
